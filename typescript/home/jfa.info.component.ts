3de48ee7bc811adf658a472a0cb061ea1db122a4
vlw0052 <leon@vlw2.com>
2016-10-05 15:53:21 -0500

    Promo picture
    HTML and CSS added for Promo picture on main page

--- typescript/home/jfa.info.component.ts
+++ typescript/home/jfa.info.component.ts
@@ -9,8 +9,13 @@ import {SettingsService} from '../services';
 	template:`	
 	<section id="{{id}}">
 		<div class="content">
-			<h2 [innerHtml]="title"></h2>
-			<p [innerHtml]="text"></p>
+		<div *ngIf="id=='how'" class="promo-img-left">
+			<img src="https://scontent-dft4-1.cdninstagram.com/t51.2885-15/e35/14295531_1149735241769034_205130704663609344_n.jpg?ig_cache_key=MTM1MDc5Mjc4NDk1NjY1MTgxNA%3D%3D.2" alt="Juggling Picture" />
+		</div>
+			<div class="message">
+				<h2 [innerHtml]="title"></h2>
+				<p [innerHtml]="text"></p>
+			</div>
 		</div>
 	</section>
 	`,
