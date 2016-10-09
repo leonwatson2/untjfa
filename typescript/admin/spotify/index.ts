aea99be403383b0ee6457de0f3cfab19b29ac7d5
vlw0052 <leon@vlw2.com>
2016-10-09 00:41:53 -0500

    File Name
    Updated referenced file name

--- typescript/admin/spotify/index.ts
+++ typescript/admin/spotify/index.ts
@@ -1,5 +1,5 @@
 
 export {SpotifySongComponent} from './spotify.song.component'
-export {SpotifyPlaylistComponent} from './spotify.playlist.component'
+export {SpotifyPlaylistComponent} from './spotify.choose-playlist.component'
 export {SpotifyLoginComponent} from './spotify.login.component'
 export {SpotifyAcceptComponent} from './spotify.accept.component'
