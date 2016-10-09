ea54dc0c56a7c32de529edb6c5c39bfa8c19201b
vlw0052 <leon@vlw2.com>
2016-10-09 00:41:24 -0500

    Download Update
    Added the ability to download photo.

--- typescript/gallery/jfa.gallery-group.component.ts
+++ typescript/gallery/jfa.gallery-group.component.ts
@@ -60,7 +60,7 @@ var grid;
 							</li>
 							<li data-info>
 								<p class="mediaDescriptionHeader"></p>
-								<p class="mediaDescriptionText">{{photo.description}}</p>
+								<p class="mediaDescriptionText"><a [href]="photo.url" download>Download This Photo</a></p>
 							</li>
 					</ul>
 				</ul>
