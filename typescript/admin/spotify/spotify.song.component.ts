a383d3490fbce6b6f8f32e63bab538ae005ec3f5
vlw0052 <leon@vlw2.com>
2016-10-18 07:36:40 -0500

    Spotify:Style Update
    Spotify admin style update

--- typescript/admin/spotify/spotify.song.component.ts
+++ typescript/admin/spotify/spotify.song.component.ts
@@ -3,7 +3,8 @@ import {Component, Input} from '@angular/core';
 @Component({
 	selector: 'spotify-song',
 	template: `
-			<h4 >{{song.name}} by {{song.artists[0].name}}</h4>
+			<h4 >{{song.name}}</h4>
+			<div>{{song.artists[0].name}}</div>
 			<div class="photo">
 				<img [src]="song?.album?.images[0].url" [alt]="song?.album?.name" />
 			</div>
