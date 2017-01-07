590eedc7f0fe988d25999ddb9b32d1f0d597d6c9
vlw0052 <leon@vlw2.com>
2017-01-07 12:00:02 -0600

    Comments and update
    Comments added
    Update on searching name pipe

--- typescript/pipes/search.pipe.ts
+++ typescript/pipes/search.pipe.ts
@@ -1,4 +1,18 @@
 import {Pipe} from '@angular/core';
+/*
+* Seach pipe that filters through an array of objects.
+* The pipe modifier is an object with a key and value.
+* Return the filtered objects that includes the value at the key
+*
+* key: the key you want to check against, default:"name"
+* value: the value you are searching for
+*
+* Ex. 
+*	<div class="search"><input type="text" [(ngModel)]="searchValue"/></div>
+*		<div *ngFor="let member of members | find:{key:'name', value:searchValue}">
+* 		<display-member-info [checkIn]="member"></display-member-info>
+*	</div>
+*/
 
 
 @Pipe({
@@ -8,11 +22,14 @@ import {Pipe} from '@angular/core';
 
 export class SearchPipe{
 	transform(pipeData, pipeModifier){
-		console.log("pipeModifier",pipeModifier);
+		console.log("pipeModifier: ", pipeModifier);
+		if(pipeModifier.key == undefined){
+			pipeModifier.key = "name";
+		}
 		return pipeData.filter( (eachItem) => {
-			return eachItem['name'].toLowerCase().includes(pipeModifier.toLowerCase())
+			return eachItem[pipeModifier.key].toLowerCase().includes(pipeModifier.value.toLowerCase())
 					||
-					eachItem['name'].toLowerCase().includes(pipeModifier.toLowerCase()); 
+					eachItem[pipeModifier.key].toLowerCase().includes(pipeModifier.value.toLowerCase()); 
 
 		});
 	}
